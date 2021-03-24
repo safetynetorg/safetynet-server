3c408c65b4e46dd9bfeadffe8fb76f0c628bb3b1
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-03-24 00:16:44 -0400

    load env files

--- internal/helpers/load_dotenv.go
+++ internal/helpers/load_dotenv.go
@@ -1,16 +1,10 @@
 package helpers
 
 import (
-	"log"
-
 	"github.com/joho/godotenv"
 )
 
 // load environment
 func LoadDotEnv() {
-	err := godotenv.Load("../../.env")
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	godotenv.Load("../../.env")
 }
