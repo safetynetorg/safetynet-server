69a718598bb840bf8df747ba970428574742d350
BenReichwein <ben.basque@icloud.com>
2021-03-23 20:39:09 -0400

    added sign up

--- internal/database/models.go
+++ internal/database/models.go
@@ -5,3 +5,8 @@ type SafetynetDevice struct {
 	Lat float64 `bson:"lat,omitempty"`
 	Lon float64 `bson:"lon,omitempty"`
 }
+
+type Email struct {
+	Id  string  `bson:"_id,omitempty"`
+	Email string `bson:"email,omitempty"`
+}
