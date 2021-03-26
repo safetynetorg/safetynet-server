23a9d4c421f51d1bc8b4d4b839dbb818f4bcb6d9
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-03-25 21:11:19 -0400

    cors fix

--- internal/server/middleware/cors.go
+++ internal/server/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 func CorsMiddleware(w http.ResponseWriter, r *http.Request) {
 
 	// change the url to "localhost:3000" if you're running locally
-	w.Header().Add("Access-Control-Allow-Origin", "https://safetynetorg.site/")
+	w.Header().Add("Access-Control-Allow-Origin", "https://safetynetorg.site")
 
 	w.Header().Add("Access-Control-Allow-Headers", "*")
 }
