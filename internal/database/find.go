0d1601ce1d0a1a37eb0b64a7d5edb959d576c0df
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-03-28 16:49:47 -0400

    only 1 email signup

--- internal/database/find.go
+++ internal/database/find.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"safetynet/internal/constants"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -17,4 +18,18 @@ func (db *db) FindDeviceById(coll string, ctx context.Context, id string) (bson.
 	}
 
 	return result, nil
+}
+
+func (db *db) SignupEmailExists(ctx context.Context, email string) (bool, error) {
+	var result bson.M
+	filter := bson.M{"email": email}
+	collection := db.Safetynet.Collection("sign-up")
+
+	if err := collection.FindOne(ctx, filter).Decode(&result); err != nil {
+		if err.Error() == constants.NO_DOC_FOUND {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
--- internal/server/handlers/sign_up.go
+++ internal/server/handlers/sign_up.go
@@ -13,6 +13,19 @@ import (
 func SignUp(w http.ResponseWriter, r *http.Request) {
 	var email database.Email
 	json.NewDecoder(r.Body).Decode(&email)
+
+	exists, err := database.Database.SignupEmailExists(context.Background(), email.Email)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if exists {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if err := database.Database.Insert(constants.SIGN_UP_COLL, context.Background(), email); err != nil {
 		http.Error(w, err.Error(), 500)
 		return
