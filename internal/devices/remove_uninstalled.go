cee18a6a82b9c4ffe3a644f218b278cd4f26f3c7
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-03 18:18:38 -0400

    close cursor

--- internal/devices/remove_uninstalled.go
+++ internal/devices/remove_uninstalled.go
@@ -2,6 +2,7 @@ package devices
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"safetynet/internal/constants"
@@ -16,6 +17,7 @@ import (
 func RemoveUninstalledDevices() {
 	ticker := time.NewTicker(30 * time.Minute)
 	ctx := context.Background()
+	var wg sync.WaitGroup
 
 	for range ticker.C {
 		devicesColl := database.Database.Safetynet.Collection(constants.DEVICES_COLL)
@@ -25,15 +27,19 @@ func RemoveUninstalledDevices() {
 			continue
 		}
 
-		defer cursor.Close(ctx)
-
 		for cursor.Next(ctx) {
-			go checkAndRemoveDevice(*cursor, ctx)
+			wg.Add(1)
+			go checkAndRemoveDevice(*cursor, ctx, &wg)
 		}
+
+		wg.Wait()
+		cursor.Close(ctx)
 	}
 }
 
-func checkAndRemoveDevice(c mongo.Cursor, ctx context.Context) {
+func checkAndRemoveDevice(c mongo.Cursor, ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	var device database.SafetynetDevice
 
 	if err := c.Decode(&device); err != nil {
