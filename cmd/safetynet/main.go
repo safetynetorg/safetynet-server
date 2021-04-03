cfa397a5b1178f0caaf7b360c505f921a631b075
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-03 17:41:03 -0400

    init fcm client

--- cmd/safetynet/main.go
+++ cmd/safetynet/main.go
@@ -5,11 +5,13 @@ import (
 	"safetynet/internal/devices"
 	"safetynet/internal/keys"
 	"safetynet/internal/server"
+	"safetynet/internal/alert"
 )
 
 func main() {
 	keys.Load()
 	database.Connect()
+	alert.InitClient()
 	go devices.RemoveUninstalledDevices()
 	server.Run()
 }
