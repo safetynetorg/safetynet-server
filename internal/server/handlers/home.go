417f8e6f8c39018f14ac388611dc0fff2c9e2a3d
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-05 16:39:09 -0400

    refactor routing

--- internal/server/handlers/home.go
+++ internal/server/handlers/home.go
@@ -1,7 +1,9 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+)
 
-func Home(w http.ResponseWriter, r *http.Request){
+func Home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to the Safetynet api"))
 }
--- internal/server/http.go
+++ internal/server/http.go
@@ -10,18 +10,17 @@ import (
 func httpInit() *cgr.Router {
 	router := cgr.NewRouter()
 	corsMiddleware := cgr.NewMiddleware(middleware.CorsMiddleware)
+	corsPreflightConf := cgr.NewRouteConf()
+	corsPreflightConf.HandlePreflight(true)
 
 	// api endpoints
 	router.Route("/").Handler(handlers.Home).Method("GET").Insert()
 	router.Route("/alert").Handler(handlers.FindDevicesToAlert).Method("POST").Insert()
 	router.Route("/new").Handler(handlers.NewDevice).Method("POST").Insert()
-	router.Route("/signup").Handler(handlers.SignUp).Assign(corsMiddleware).Method("POST").Insert()
-	router.Route("/contact").Handler(handlers.Contact).Assign(corsMiddleware).Method("POST").Insert()
+	router.Route("/signup").Handler(handlers.SignUp).Assign(corsMiddleware).SetConf(corsPreflightConf).Method("POST", "OPTIONS").Insert()
+	router.Route("/contact").Handler(handlers.Contact).Assign(corsMiddleware).SetConf(corsPreflightConf).Method("POST", "OPTIONS").Insert()
 	router.Route("/updatelocation").Handler(handlers.UpdateLocation).Method("PUT").Insert()
 	router.Route("/delete/:id").Handler(handlers.DeleteDevice).Method("DELETE").Insert()
-
-	// cors preflight
-	corsPreflight(router, corsMiddleware)
 
 	router.Route("/viewroutes").Handler(func(w http.ResponseWriter, r *http.Request) {
 		for _, route := range router.ViewRouteTree() {
@@ -31,11 +30,3 @@ func httpInit() *cgr.Router {
 
 	return router
 }
-
-func corsPreflight(router *cgr.Router, middleware *cgr.Middleware) {
-
-	router.Route("/signup").Handler(ok).Assign(middleware).Method("OPTIONS").Insert()
-	router.Route("/contact").Handler(ok).Assign(middleware).Method("OPTIONS").Insert()
-}
-
-func ok(rw http.ResponseWriter, r *http.Request) { rw.WriteHeader(http.StatusOK) }
