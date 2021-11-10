09016c876d42a681950aa0678e79665cbd54ed36
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-11-10 10:45:57 -0500

    fixed some casing

--- internal/location/distance_between.go
+++ internal/location/distance_between.go
@@ -15,20 +15,20 @@ type coordPair struct {
 
 // check if the distance between two coordinates is within [constants.ALERT_RADIUS]
 func checkInDistance(coords coordPair) bool {
-	distance_between := distanceBetweenCoords(coords)
+	distanceBetween := distanceBetweenCoords(coords)
 
-	return distance_between <= constants.ALERT_RADIUS
+	return distanceBetween <= constants.ALERT_RADIUS
 }
 
 // find the distance between two coordinated (in km)
 func distanceBetweenCoords(coords coordPair) float64 {
-	delta_lat := helpers.AsRadians(coords.LatRecv - coords.LatSrc)
-	delta_lon := helpers.AsRadians(coords.LonRecv - coords.LonSrc)
+	deltaLat := helpers.AsRadians(coords.LatRecv - coords.LatSrc)
+	deltaLon := helpers.AsRadians(coords.LonRecv - coords.LonSrc)
 
-	a := math.Sin(delta_lat/2)*math.Sin(delta_lat/2) +
+	a := math.Sin(deltaLat/2)*math.Sin(deltaLat/2) +
 		math.Cos(helpers.AsRadians(coords.LatSrc))*
 			math.Cos(helpers.AsRadians(coords.LatRecv))*
-			math.Sin(delta_lon/2)*math.Sin(delta_lon/2)
+			math.Sin(deltaLon/2)*math.Sin(deltaLon/2)
 
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 	d := constants.EARTH_RADIUS * c
